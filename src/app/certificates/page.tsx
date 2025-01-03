7225a6b3c5ed07d744863cc83fd745bcf89c9e7e
leconstantin <lcon69184@gmail.com>
2025-01-03 13:47:15 +0200

    Test changes

--- src/app/certificates/page.tsx
+++ src/app/certificates/page.tsx
@@ -11,7 +11,7 @@ export default function Certificates() {
         <div className="pt-14 sm:pt-18">
           <div className="max-w-2xl px-1">
             <h1 className="text-4xl font-bold font-roboto  tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-              Professional Certifications
+              Professional Certification
             </h1>
             <p className="mt-6 text-base font-roboto text-zinc-600 dark:text-zinc-300">
               A Comprehensive Collection of Industry-Recognized Certifications
